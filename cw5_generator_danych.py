e68328a3eb26800a6a30e4ff86cc68dd83ec9859
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-12-07 15:06:16 +0100

    Generator danych

---
+++ cw5_generator_danych.py
@@ -0,0 +1,20 @@
+from faker import Faker
+import random
+
+# inicjalizacja generatora Faker
+fake = Faker()
+
+def generuj_dane_testowe(liczba_uzytkownikow):
+    dane = []
+    for i in range(liczba_uzytkownikow):
+        uzytkownik = {
+            "imie": fake.first_name(),
+            "email": fake.email(),
+            "wiek": random.randint(18,60)
+        }
+        dane.append(uzytkownik)
+    return dane
+
+
+#wygeneruje 10 roznych uzykownikow
+print(generuj_dane_testowe(100))
