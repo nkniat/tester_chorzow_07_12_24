262746e5ac9f78bf360a76eeabafa492003e52f4
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-12-07 13:45:27 +0100

    Zadanie pierwsze

---
+++ cw1_operacje.py
@@ -0,0 +1,11 @@
+# pobranie danych
+num1 = float(input("Podaj pierwsza liczbe "))
+num2 = float(input("Podaj druga liczbe "))
+
+# operacje na liczbach
+suma = num1 + num2
+potegowanie = num1 ** num2
+
+# wyswietlenie wyniku
+print(f"Suma Twoich liczb {num1}, {num2} to: {suma}")
+print(f"{num1} do potegi {num2} to: {potegowanie}")
