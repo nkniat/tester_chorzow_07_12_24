4e7b4814cef6edc24959f16b73e57238c58ad7f1
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-12-07 14:16:07 +0100

    Typy zlozone

---
+++ cw2_listy_slowniki.py
@@ -0,0 +1,10 @@
+# lista owocow
+owoce = ["banan", "jablko", "mandarynka"]
+
+# slownik z cenami
+ceny = {"banan": 4, "jablko": 2, "mandarynka": 5}
+
+#wyswietlenie wynikow
+#print(ceny)
+ulubiony = owoce[2]  #jablko
+print(f"Mój ulubiony owoc to {ulubiony} i kosztuje {ceny[ulubiony]} zł")
